ed8d7a0572e7e634541d9aa3f75a771f0268b0c6
vladvonvlad <sebastienalain.roy@gmail.com>
2020-06-29 14:35:35 +0000

    configure api invokeURL

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-1
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://2g9s661ko9.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
