32bc78628694166248a9a9c0fcc1f76afdb4dae0
vladvonvlad <sebastienalain.roy@gmail.com>
2020-06-29 12:52:15 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: '', // e.g. us-east-1_2V4Ogy1mg 
+        userPoolClientId: '', // e.g. 4h3ffpv2q0a1gk68ht4iguegra
+        region: '' // e.g. us-east 1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
